c196bd2334893aadb6d410d6868e5272711170f1
arpadzsoltjakab <arpadzsoltjakab@gmail.com>
2016-10-10 22:20:52 +0200

    refactor + bugfix

--- to-do-app/to_do_app.py
+++ to-do-app/to_do_app.py
@@ -1,49 +1,54 @@
+import re
+
 def print_lines(lines):
     print("You saved the following to-do items:")
     for i in range(len(lines)):
         print("\t" + str(i + 1) + ". " + lines[i], end="")
 
 def read_database_lines():
-    database = open("database.txt", "r")
+    database = open("database.txt", "a+")
+    database.seek(0, 0)
     lines = database.readlines()
     database.close()
     return lines
 
-def write_database_lines()
+def write_database_lines(lines):
     database = open("database.txt", "w+")
     database.writelines(lines)
     database.close()    
 
 def list_items():
-    print_lines(read_database_lines())
+    lines = read_database_lines()
+    print_lines(lines) 
 
 def add():
-    database = open("database.txt", "a")
     item = input("Add an item: ")
-    database.write("[ ] " + item + "\n")
+    lines = read_database_lines()
+    lines.append("[ ] " + item + "\n")
+    write_database_lines(lines)
     print("Item added.")
-    database.close()
-
+    
 def mark():
     lines = read_database_lines()
-    print_lines(lines)
-    
-    comleted_item_index = int(input("Which one you want to mark as completed: "))
-    
-    if comleted_item_index <= len(lines):
-        lines[comleted_item_index - 1] = lines[comleted_item_index - 1][:1] + "x" + lines[comleted_item_index - 1][2:]
-        print(lines[comleted_item_index - 1][3:-1] + " is completed")  
-
-    write_database_lines()
+    print_lines(lines)   
+    comleted_item_index = input("Which one you want to mark as completed: ")
+    pattern = re.compile("^-?[0-9]+$")
+    if pattern.match(comleted_item_index):
+        comleted_item_index = int(comleted_item_index)
+        if comleted_item_index <= len(lines):
+            lines[comleted_item_index - 1] = lines[comleted_item_index - 1][:1] + "x" + lines[comleted_item_index - 1][2:]
+            print(lines[comleted_item_index - 1][3:-1] + " is completed")  
+            write_database_lines(lines)
 
 def archive():
-    lines = read_database_lines():
+    all_lines = read_database_lines()
 
-    database = open("database.txt", "w+")
-    for i in range(len(lines)):
-        if lines[i][1] != 'x':
-            database.write(lines[i])
-    database.close()
+    kept_lines = []
+    for line in all_lines:
+        if line[1] != 'x':
+            kept_lines.append(line)
+
+    write_database_lines(kept_lines)
     print("All completed tasks got deleted.")    
 
 command = input("Please specify a command [list, add, mark, archive]: ")
