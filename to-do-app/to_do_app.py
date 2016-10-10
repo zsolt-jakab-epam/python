5293a0326cacdc877aa81ebf708dae51c6858a77
Zsolt Jakab <zsolt_jakab@epam.com>
2016-10-10 13:44:24 +0200

    add to do app spine

---
+++ to-do-app/to_do_app.py
@@ -0,0 +1,14 @@
+import sys
+
+
+database = open("database.txt", "a+")
+
+command = input("Please specify a command [list, add, mark, archive]: ")
+
+if command == "list":
+
+elif command == "add":
+
+elif command == "mark":    
+
+elif command == "archive":      
